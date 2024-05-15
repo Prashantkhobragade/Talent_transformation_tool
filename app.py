58bfc65504e817b94874716399c366ca405b6671
Prashantkhobragade <prashantkhobragade011@gmail.com>
2024-05-15 10:22:09 +0530

    port udated

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ async def get_courses(skills: str, experience: float):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8010)
